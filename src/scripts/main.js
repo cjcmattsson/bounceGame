c89e881113ac7563cfe3eed2857020e5f6613033
cjcmattsson <cjcmattsson@gmail.com>
2018-02-16 12:42:47 +0100

    Setup map structure and installed npm packages

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+var canvas = document.querySelector("#myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
